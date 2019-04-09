103d8b2f4ec02bdfa730d506f41bbe9f10c257ba
Conchristatdor <christopherdsloat@gmail.com>
2019-04-08 20:19:42 -0400

    fixed typo in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-from flask import jsonifyssss
+from flask import jsonify
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/Terroist"
